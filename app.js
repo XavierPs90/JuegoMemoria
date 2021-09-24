c48bf1c694d12590abb16d725c573049a9c908e0
XavierPs90 <xavialonsops@gmail.com>
2021-09-24 13:57:24 -0600

    Se agrego cambio de idioma

--- app.js
+++ app.js
@@ -1,19 +1,62 @@
-let iconos = [];
-let selecciones = [];
+let icons = [];
+let selecctions = [];
 let cont = 0;
-let btn = document.querySelectorAll("button");
 let level = 12;
 let lastLevel;
+let gradient = "background: linear-gradient(hsl(34, 82%, 57%), hsl(34, 82%, 54%));"
+let gradientHover = "background: linear-gradient(hsl(34, 82%, 47%), hsl(34, 82%, 44%));";
+let btn = document.querySelectorAll("button");
+let h1 = document.querySelector("h1");
+let english = document.querySelector(".english");
+let spanish = document.querySelector(".spanish");
+let ad;
+let languageCheck = document.querySelector(".language");
+let languageSelected = window.navigator.language;
+
+english.textContent = "English";
+spanish.textContent = "Español";
+(languageSelected == "en-US") ? languageCheck.checked = false : true;
+
+let chargeData = (languageSelected) => {
+    if(languageSelected == "en-US")
+    {
+        document.title = "Memory Game";
+        h1.textContent = "Memory Game";
+        btn[0].textContent = "Easy";
+        btn[1].textContent = "Medium";
+        btn[2].textContent = "Hard";
+        btn[3].textContent = "Mix Cards";
+        ad = "¡Congratulations, you have won!. Play again";
+    }
+    else
+    {
+        document.title = "Juego de Memoria";
+        h1.textContent = "Juego de Memoria";
+        btn[0].textContent = "Fácil";
+        btn[1].textContent = "Intermedio";
+        btn[2].textContent = "Dificil";
+        btn[3].textContent = "Mezclar Tarjetas";
+        ad = "¡Felicidades, has ganado!. Juega otra vez";
+    }
+}
+
+languageCheck.addEventListener("click", () => {
+    languageSelected = (languageCheck.checked) ? "es" : "en-US";
+
+    chargeData(languageSelected);
+});
+
+chargeData(languageSelected);
 
 btn.forEach(function (item){
-    item.addEventListener("click", function() {
+    item.addEventListener("click", () => {
         let selectedLevel = item.dataset.level || lastLevel;
         createPanel(selectedLevel);
     })
 })
 
 function loadIcons(){
-    iconos = [
+    icons = [
         '<img src="img/2x/baseball.png" alt="Icono de baseball">',
         '<img src="img/2x/basket.png" alt="Icono de basket">',
         '<img src="img/2x/control.png" alt="Icono de control">',
@@ -26,94 +69,93 @@ function loadIcons(){
         '<img src="img/2x/softball.png" alt="Icono de softball">',
         '<img src="img/2x/tenis.png" alt="Icono de tenis">',
         '<img src="img/2x/volleyball.png" alt="Icono de volleyball">',
-    ]
+    ];
 }
 
 function createPanel(selectedLevel){
-    selecciones = [];
+    selecctions = [];
     loadIcons();
     
     if(selectedLevel == 12){
         
-        btn[0].setAttribute("style", "background: linear-gradient(hsl(34, 82%, 47%), hsl(34, 82%, 44%));")
-        btn[1].setAttribute("style", "background: linear-gradient(hsl(34, 82%, 57%), hsl(34, 82%, 54%));")
-        btn[2].setAttribute("style", "background: linear-gradient(hsl(34, 82%, 57%), hsl(34, 82%, 54%));")
+        btn[0].setAttribute("style", gradientHover);
+        btn[1].setAttribute("style", gradient);
+        btn[2].setAttribute("style", gradient);
     }
 
     if(selectedLevel == 18){
-        btn[0].setAttribute("style", "background: linear-gradient(hsl(34, 82%, 57%), hsl(34, 82%, 54%));")
-        btn[1].setAttribute("style", "background: linear-gradient(hsl(34, 82%, 47%), hsl(34, 82%, 44%));")
-        btn[2].setAttribute("style", "background: linear-gradient(hsl(34, 82%, 57%), hsl(34, 82%, 54%));")
+        btn[0].setAttribute("style", gradient);
+        btn[1].setAttribute("style", gradientHover);
+        btn[2].setAttribute("style", gradient);
     }
 
     if(selectedLevel == 24){
-        btn[0].setAttribute("style", "background: linear-gradient(hsl(34, 82%, 57%), hsl(34, 82%, 54%));")
-        btn[1].setAttribute("style", "background: linear-gradient(hsl(34, 82%, 57%), hsl(34, 82%, 54%));")
-        btn[2].setAttribute("style", "background: linear-gradient(hsl(34, 82%, 47%), hsl(34, 82%, 44%));")
+        btn[0].setAttribute("style", gradient);
+        btn[1].setAttribute("style", gradient);
+        btn[2].setAttribute("style", gradientHover);
     }
 
     level = selectedLevel;
     lastLevel = level;
     
-    let panel = document.getElementById("tablero")
-    let tarjetas = []
+    let panel = document.getElementById("panel")
+    let cards = []
 
     for (let i = 0; i < selectedLevel; i++){
-        tarjetas.push(
-            `<div class="area-tarjeta" onclick="selectCard(${i})">
-                <div class="tarjeta" id="tarjeta${i}">
-                    <div class="cara trasera" id="trasera${i}">
-                        ${iconos[0]}
+        cards.push(
+            `<div class="card-area" onclick="selectCard(${i})">
+                <div class="card" id="card${i}">
+                    <div class="card-face back-face" id="back-face${i}">
+                        ${icons[0]}
                     </div>
-                    <div class="cara superior">
+                    <div class="card-face front-face">
                         <img src="img/2x/ask.png" alt="Signo de pregunta">
                     </div>
                 </div>
             </div>`
         )
         if (i % 2 == 1){
-            iconos.splice(0,1)
+            icons.splice(0,1)
         }
     }
-    tarjetas.sort(() => Math.random() - 0.5)
-    panel.innerHTML = tarjetas.join(" ")
+    cards.sort(() => Math.random() - 0.5)
+    panel.innerHTML = cards.join(" ")
 }
 
 function selectCard(i){
-    let tarjeta = document.getElementById("tarjeta"+i)
+    let card = document.getElementById("card"+i)
     
-    if(tarjeta.style.transform != "rotateY(180deg)"){
-        tarjeta.style.transform = "rotateY(180deg)"
-        selecciones.push(i)
+    if(card.style.transform != "rotateY(180deg)"){
+        card.style.transform = "rotateY(180deg)"
+        selecctions.push(i)
     }
 
-    if(selecciones.length == 2){
-        deseleccionar(selecciones)
-        selecciones = []
+    if(selecctions.length == 2){
+        deselect(selecctions)
+        selecctions = []
     }
 }
 
-function deseleccionar(selecciones) {
+function deselect(selections) {
     setTimeout(() => {
-        let trasera1 = document.getElementById("trasera" + selecciones[0])
-        let trasera2 = document.getElementById("trasera" + selecciones[1])
+        let backFace1 = document.getElementById("back-face" + selections[0])
+        let backFace2 = document.getElementById("back-face" + selections[1])
         
-        if(trasera1.innerHTML != trasera2.innerHTML){
-            let tarjeta1 = document.getElementById("tarjeta" + selecciones[0])
-            let tarjeta2 = document.getElementById("tarjeta" + selecciones[1])
-            tarjeta1.style.transform = "rotateY(0deg)"
-            tarjeta2.style.transform = "rotateY(0deg)" 
+        if(backFace1.innerHTML != backFace2.innerHTML){
+            let card1 = document.getElementById("card" + selections[0])
+            let card2 = document.getElementById("card" + selections[1])
+            card1.style.transform = "rotateY(0deg)"
+            card2.style.transform = "rotateY(0deg)" 
         }
         else{
-            trasera1.style.background = "plum"
-            trasera2.style.background = "plum"
+            backFace1.style.background = "plum"
+            backFace2.style.background = "plum"
             cont += 2;
             
             if(cont == level){
-                alert("¡Felicidades, has ganado!. Vuelve empezar");
+                alert(ad);
                 cont = 0
             }
         }
     }, 500);
 }
-
